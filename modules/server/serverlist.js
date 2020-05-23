b9f8498cd8fb7eb279912e9661eff65ce359103b
Etienne Cadillon <djctavia@gmail.com>
2020-05-23 02:39:53 +0200

    Create serverlist.js

---
+++ modules/server/serverlist.js
@@ -0,0 +1,47 @@
+const { v4: uuidv4 } = require('uuid');
+const REGEXSERVER = /game-[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12}/;
+
+
+class ServerList {
+    constructor() {
+        this.servers = new Map();
+    }
+
+    GetPlayers({rooms}) {
+        if (typeof rooms !== 'object') return undefined;
+        const server = this.servers.get(rooms.find(room => room.match(REGEXSERVER)));
+        if (!server) return undefined;
+        return server.players;
+    }
+}
+
+class Server {
+    constructor({id}, name = "default", password = "")
+    {
+        this.id = `game-${uuidv4()}`;
+        this.name = name;
+        this.password = password;
+        this.hoster = id;
+        this.players = [id];
+        this.canJoin = true;
+        console.log(`[Server] UID: ${this.id} | name: ${this.name} | pwd: ${this.password} | Hoster: ${this.hoster}`);
+    }
+
+    IsPlayerAlreadyInAGame(rooms) {
+        if (typeof rooms !== 'object') return undefined;
+        return rooms.some(room => room.match(REGEXSERVER));
+    }
+
+    AddPlayer({id: playerId, rooms, join}) {
+        if (this.IsPlayerAlreadyInAGame(rooms)) return;
+        this.players.push(playerId);
+        join(this.id);
+    }
+}
+Server.prototype.toString = () => `[Server] UID: ${this.id} | name: ${this.name} | pwd: ${this.password} | Hoster: ${this.hoster}`;
+
+const gServerList = new ServerList();
+module.exports = {
+    ServerList: gServerList,
+    Server
+};
