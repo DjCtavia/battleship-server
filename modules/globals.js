4191352c39de6d0eea6922c735b1e5d3ce88586a
Etienne Cadillon <djctavia@gmail.com>
2020-05-24 16:02:03 +0200

    Creation of server has been moved

--- modules/globals.js
+++ modules/globals.js
@@ -4,13 +4,15 @@ const socketIo = require('socket.io');
 
 // Config
 const PORT = 3001;
-const INDEX = require("./routes/index");
+const INDEX = require("../routes/index");
 // Express init
 const app = express();
 app.use(INDEX);
 // Socket Io init
 const server = http.createServer(app);
 const io = socketIo(server);
+
+server.listen(PORT, () => console.log(`Listening port ${PORT}`));
 
 module.exports = {
     express,
